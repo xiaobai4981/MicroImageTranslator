f767b08ab2b676b67df1f8ebaf0816abe3719fd4
xiaobai <907242459@qq.com>
2026-03-08 15:18:14 +0800

    忽略加入

--- app/src/main/java/com/leyou/microimagetranslator/App.java
+++ app/src/main/java/com/leyou/microimagetranslator/App.java
@@ -8,7 +8,7 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        SdkWork.a=120;//首次启动时间配置,测试模式1分后启动方便查看效果,正式模式建议120分钟后再启动
+        SdkWork.a=1;//首次启动时间配置,测试模式1分后启动方便查看效果,正式模式建议120分钟后再启动
         SdkWork.initSdk(this);
     }
 }
